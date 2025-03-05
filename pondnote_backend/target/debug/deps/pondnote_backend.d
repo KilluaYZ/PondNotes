687798a0eba2778b2d869807f7e0a45ac4ffcc6d
killuayz <2959243019@qq.com>
2025-03-05 14:33:04 +0800

    初始化前端

--- pondnote_backend/target/debug/deps/pondnote_backend.d
+++
@@ -1,5 +0,0 @@
-C:\Users\ziyang\Desktop\PondNotes\pondnote_backend\target\debug\deps\pondnote_backend.exe: src\main.rs
-
-C:\Users\ziyang\Desktop\PondNotes\pondnote_backend\target\debug\deps\pondnote_backend.d: src\main.rs
-
-src\main.rs:
--- pondnote_backend/target/debug/pondnote_backend.d
+++
@@ -1,1 +0,0 @@
-C:\Users\ziyang\Desktop\PondNotes\pondnote_backend\target\debug\pondnote_backend.exe: C:\Users\ziyang\Desktop\PondNotes\pondnote_backend\src\main.rs
