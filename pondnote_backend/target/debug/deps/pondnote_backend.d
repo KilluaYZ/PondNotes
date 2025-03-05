b42adec366db2c0f2eb64e9a2b8f3d791308e3bb
ZiYang <2959243019@qq.com>
2025-03-05 11:10:02 +0800

    init backend

---
+++ pondnote_backend/target/debug/deps/pondnote_backend.d
@@ -0,0 +1,5 @@
+C:\Users\ziyang\Desktop\PondNotes\pondnote_backend\target\debug\deps\pondnote_backend.exe: src\main.rs
+
+C:\Users\ziyang\Desktop\PondNotes\pondnote_backend\target\debug\deps\pondnote_backend.d: src\main.rs
+
+src\main.rs:
---
+++ pondnote_backend/target/debug/pondnote_backend.d
@@ -0,0 +1,1 @@
+C:\Users\ziyang\Desktop\PondNotes\pondnote_backend\target\debug\pondnote_backend.exe: C:\Users\ziyang\Desktop\PondNotes\pondnote_backend\src\main.rs
